99c2eeba37fcb5f42f968480d460a091d3a50d77
Bojtronic <bojtronic@gmail.com>
2025-01-28 19:48:38 -0600

    Implementación del mockup en el servidor

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require('dotenv').config(); 
 const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000; 
 
 // Configurar la carpeta "public" para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Redirigir la raíz a `/pages/gestion_cultural.html`
+// Ruta principal para enviar el archivo HTML
 app.get('/', (req, res) => {
-  res.redirect('/pages/gestion_cultural.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Iniciar el servidor
--- public/js/load_site.js
+++ public/js/load_site.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("../pages/gestion_cultural.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("home").innerHTML = data;
+    const menuLinks = document.querySelectorAll(".menu a");
+
+    menuLinks.forEach(link => {
+        link.addEventListener("click", function (event) {
+        event.preventDefault(); // Evita la navegación predeterminada
+
+        const targetPage = this.getAttribute("href"); // Obtiene la URL del enlace
+        window.location.href = `${window.location.origin}/${targetPage}`;
         });
+    });
 });
